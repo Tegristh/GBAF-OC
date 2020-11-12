be16e30b00b30d5a27b0088bed0d146564cf7d81
Tegristh <63445829+Tegristh@users.noreply.github.com>
2020-11-12 15:58:15 +0100

    12nov01
    taff initial (html brut)

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf8">
+<link href="styles/default.css" rel="stylesheet"/>
+</head>
+<body>
+<header>
+<img src="images/LOGOGBAF.png" alt="Logo GBAF"/>
+<div class="user-connected">
+<img src="#" alt="user avatar"/>
+<p>Nom & Prenom</p>
+<div>
+</header>
+<div class="partenaire">
+<h1>Groupement banque assurance Francais</h1>
+<p>Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français:</p>
+<ul>
+<li>BNP Paribas;</li>
+<li>BPCE;</li>
+<li>Crédit Agricole;</li>
+<li>Credit Mutuel-CIC;</li>
+<li>Société Générale;</li>
+<li>La Banque Postale.</li>
+</ul>
+<p>Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l'echelle nationale. C'est aussi un interlocuteur privilégié des pouvoirs publics.</p>
+<img src="images/illustrations/la_defense.jpg" alt="La défense quartier d'affaires parisien"/>
+<h2>Nos partenaires, les acteurs du secteur bancaire</h2>
+<p>liste des partenaires et acteurs du groupe, produits et services bancaires et financiers</p>
+
+
+</body>
+</html>
