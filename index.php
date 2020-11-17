c87c8cd984d0adcfdb31f5a0e2613ffb3b6205e4
Tegristh <63445829+Tegristh@users.noreply.github.com>
2020-11-17 07:09:05 +0100

    responsive cards
    commit avant pause.

--- index.php
+++ index.php
@@ -54,27 +54,90 @@ l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur p
 pouvoirs publics.</p>
             </div>
         </div> 
-</div>
-<div class="container">
+    </div>
+    <div class="container">
         <div class="row">
             <div class="col">
                 <h2 class="text-center">Les partenaires et financeurs</h2>
                 <p class="text-justify">Les produits et services bancaires sont nombreux et très variés. Le GBAF souhaite proposer aux salariés des grands groupes Français 
                     un point d'entrée unique, répertoriant un grand nombre d'informations sur les partenaires et acteurs 
                     du groupe ainsi que sur les produits et services bancaires et financiers.</p>
-    </div>
-</div> 
-</div>          
-    </div>
+            </div>
+         </div> 
+    </div>          
+    
     <div class="container acteurs">
-        
         <div class="row">
+            <!--Début de carte acteur -->
             <div class="card col-12 ">
-                <div class="card-body">
-                    <div class="col-md-6"><img src="images/logo-partenaire/CDE.png"class="img-fluid"  alt="Chambre des entrepreneurs"></div>
-                    <div class="col-md-6"><h3 class="card-title  text-center text-md-left">Chambre des entrepreneurs</h3></div>
-                    <p class="card-text">Contenu textuel de la carte</p>
+                <div class="row ">
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <img src="images/logo-partenaire/CDE.png" class="img-fluid" alt="chambre des entrepreneurs logo">
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-8 ">
+                        <h3 class="text-align">Chambre des entrepreneurs</h3>
+                        <p class="text-justify">La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. 
+Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.</p>
+                    </div>
+                    <div class="col-12 col-lg-2 ">
+                        <button type="button" class="btn btn-outline-info">Lire la suite</button>
+                    </div>
                 </div>
+            </div>
+            <!-- fin de carte acteur -->
+                 <!--Début de carte acteur -->
+            <div class="card col-12 ">
+                <div class="row ">
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <img src="images/logo-partenaire/formation_co.png" class="img-fluid" alt="Formation&co logo">
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-8 ">
+                        <h3 class="text-align">Formation&co</h3>
+                        <p class="text-justify">Formation&co est une association française présente sur tout le territoire.
+Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.</p>
+                    </div>
+                    <div class="col-12 col-lg-2 ">
+                    <button type="button" class="btn btn-outline-info">Lire la suite</button>
+                    </div>
+                </div>
+            </div>
+            <!-- fin de carte acteur -->
+             <!--Début de carte acteur -->
+             <div class="card col-12 ">
+                <div class="row ">
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <img src="images/logo-partenaire/Dsa_france.png" class="img-fluid" alt="DSA France logo">
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-8 ">
+                        <h3 class="text-align">DSA France</h3>
+                        <p class="text-justify">Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.
+Nous accompagnons les entreprises dans les étapes clés de leur évolution.
+Notre philosophie : s’adapter à chaque entreprise.
+Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises</p>
+                    </div>
+                    <div class="col-12 col-lg-2 ">
+                    <button type="button" class="btn btn-outline-info">Lire la suite</button>
+                    </div>
+                </div>
+            </div>
+            <!-- fin de carte acteur -->
+             <!--Début de carte acteur -->
+             <div class="card col-12 ">
+                <div class="row ">
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <img src="images/logo-partenaire/protectpeople.png" class="img-fluid" alt="Protectpeople">
+                    </div>
+                    <div class="col-12 col-md-8 col-lg-8 ">
+                        <h3 class="text-align">Protectpeople</h3>
+                        <p class="text-justify">Protectpeople finance la solidarité nationale.
+Nous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.</p>
+                    </div>
+                    <div class="col-12 col-lg-2 ">
+                    <button type="button" class="btn btn-outline-info">Lire la suite</button>
+                    </div>
+                </div>
+            </div>
+            <!-- fin de carte acteur -->
             </div>
         </div>
     </div>
