75c4295fc5ddb94da8d36e0f45dffcc3753dc396
Tegristh <63445829+Tegristh@users.noreply.github.com>
2020-11-17 04:57:48 +0100

    bootstrap convert
    convert to bootstrap css for fast prototyping.

--- index.php
+++ index.php
@@ -1,33 +1,90 @@
 <!doctype html>
-<html>
 <head>
-<meta charset="utf8">
-<link href="styles/default.css" rel="stylesheet"/>
+    <title>GBAF - Groupement Banque Assurance Français</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="styles/default.css" rel="stylesheet">
+    <meta charset="utf8" langue="fr">
 </head>
 <body>
-<header>
-<img src="images/LOGOGBAF.png" alt="Logo GBAF"/>
-<div class="user-connected">
-<img src="#" alt="user avatar"/>
-<p>Nom & Prenom</p>
-<div>
-</header>
-<div class="partenaire">
-<h1>Groupement banque assurance Francais</h1>
-<p>Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grands groupes français:</p>
-<ul>
-<li>BNP Paribas;</li>
-<li>BPCE;</li>
-<li>Crédit Agricole;</li>
-<li>Credit Mutuel-CIC;</li>
-<li>Société Générale;</li>
-<li>La Banque Postale.</li>
-</ul>
-<p>Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l'echelle nationale. C'est aussi un interlocuteur privilégié des pouvoirs publics.</p>
-<img src="images/illustrations/la_defense.jpg" alt="La défense quartier d'affaires parisien"/>
-<h2>Nos partenaires, les acteurs du secteur bancaire</h2>
-<p>liste des partenaires et acteurs du groupe, produits et services bancaires et financiers</p>
+    <div class="container header">
+        <div class="row">
+            
+             <nav class="col navbar navbar-expand-lg bg-light navbar-light">
+                 <a class="navbar-brand" href="#">
+                     <img src="images/LOGOGBAF.png" width="40" height="40" alt="site logo">
+                     GBAF - Groupement Banque Assurance Français</a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+                     <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="navbarContent" class="collapse navbar-collapse">
+                 <!-- <ul  class="navbar-nav">
+                     <li class="nav-item active">
+                         <a class="nav-link" href="index.html">Accueil</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="#">Cours</a>
+                     </li>
+                 </ul> -->
+                </div>
+             </nav>                                  
+            
+           
+        </div>
+    </div>
+    <div class="container jumbotron">
+        <div class="row">
+            <div class="col title">
+                <h1>Présentation du Groupement Banque Assurance Français</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col description">
+                <p>Le Le Groupement Banque Assurance Français (GBAF) est une fédération représentant les 6 grand groupes Français:</p>
+                <ul>
+                    <li>BNP Paribas;</li>
+                    <li>BPCE;</li>
+                    <li>Crédit Agricole;</li>
+                    <li>Crédit Mutuel-CIC;</li>
+                    <li>Société Générale;</li>
+                    <li>La Banque Postale.</li>
+                </ul>
+            </div>
+        </div>            
+    </div>
+    <div class="container infocard-section">
+        <div class="row">
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <h5 class="card-title">Titre de la carte</h5>
+                    <p class="card-text">Contenu textuel de la carte</p>
+                    </div>
+            </div>
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <h5 class="card-title">Titre de la carte</h5>
+                    <p class="card-text">Contenu textuel de la carte</p>
+                    </div>
+            </div>
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <h5 class="card-title">Titre de la carte</h5>
+                    <p class="card-text">Contenu textuel de la carte</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div class="container footer">
+        <div class="row">
+            <div class="col">
+                <p>legals | contacts</p>
+            </div>
+        </div>
+    </div>
 
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 </html>
