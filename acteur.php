70276d86b0b8edb9331997b782e087e0f2d86ba6
Baptiste Giannesini <b.giannesini@gmail.com>
2020-12-16 18:40:42 +0100

    Vanilla css day 2 commit 1 page acteur.php

--- acteur.php
+++ acteur.php
@@ -9,7 +9,7 @@ $donnees = $req->fetch();
 $nom_page= $donnees['acteur']; 
 include('Head.php'); ?>
         <!-- Début présentation de l'acteur -->
-        <div class="presentation">         
+        <div class="presentation-acteur">         
             <div>
                 <?php echo '<img src="http://gbaf.tegristh.fr/img/part/'.$donnees['id_acteur'].'.'.$donnees['logo'].'" alt="logo">'; ?>
             </div>
@@ -26,95 +26,83 @@ include('Head.php'); ?>
         <!-- Fin présentation de l'acteur -->
 
 
-        <!-- début partie commentaires/notes -->
-        <div class="">
-<!-- compteur de commentaires -->
-            <div class="col-3">
-                <?php 
-                $nb_commentaires = $bdd->prepare('SELECT COUNT(*) AS nb_commentaires FROM post WHERE id_acteur = :acteur_id');
-                $nb_commentaires->execute(array('acteur_id'=>$_GET['acteur']));
-                $comment_count = $nb_commentaires->fetch();
+        <div class="big-box" >
+            <div class="interact" >
+<!-- Nombre de commentaires -->
+                <div class="nb_commentaires" >
+                    <?php 
+                        $nb_commentaires = $bdd->prepare('SELECT COUNT(*) AS nb_commentaires FROM post WHERE id_acteur = :acteur_id');
+                        $nb_commentaires->execute(array('acteur_id'=>$_GET['acteur']));
+                        $comment_count = $nb_commentaires->fetch();
 
-                echo '<p class="text-left align-middle">'.$comment_count['nb_commentaires'].' Commentaires</p>'; 
-                ?>
-            </div>
-<!-- bouton pour commenter -->
-            <div class="col-3">
-                    <button type="button" class="btn btn-outline-secondary">
-                        Laisser un commentaire
-                    </button>
-            </div>
-<!-- module satisfaction -->                
-            <div class="col-3 ">
-                <?php
-                $nb_votes = $bdd->prepare('SELECT COUNT(*) AS nb_votes FROM vote WHERE id_acteur = :acteur_id');
-                $nb_votes->execute(array('acteur_id'=>$_GET['acteur']));
-                $vote_count = $nb_votes->fetch();
-
-                $satisfaction = $bdd->prepare('SELECT AVG (vote) as vote_moyen FROM vote WHERE id_acteur = :acteur_id');
-                $satisfaction->execute(array('acteur_id'=>$_GET['acteur']));
-                $note = $satisfaction->fetch();
-                echo '<p>'.round($note['vote_moyen']*100).'% d\'utilisateurs satisfaits <br/>'.$vote_count['nb_votes'].' votes exprimés </p>';
-               
-                ?>
-            </div>
-<!-- module like/dislike -->
-            <div class="btn-group col-3 align-self-center" role="social" aria-label="Caesar-choice">
-                    
-                    <button type="button" class="btn btn-outline-success rounded">
-                    <i class="fas fa-thumbs-up"></i>
-                    </button>
-                    <button type="button" class="btn btn-outline-danger rounded">
-                    <i class="fas fa-thumbs-down"></i>
-                    </button>
-            </div>
-<!-- Module affiche commentaires -->            
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                <p> 10 derniers commentaires: </p>
-                    </div>
+                        echo '<p>'.$comment_count['nb_commentaires'].' Commentaires </p>'; 
+                    ?>
                 </div>
-                <div class="card">
-                    <div class="row">
-                        <?php
+<!-- bouton commenter -->
+                <div class="commenter-bouton" >
+                    <button type="button" class="bouton2">
+                        Commenter
+                    </button>
+                </div>
+<!-- module satisfaction -->
+                <div class="satisfaction" >
+                    <?php
+                        $nb_votes = $bdd->prepare('SELECT COUNT(*) AS nb_votes FROM vote WHERE id_acteur = :acteur_id');
+                        $nb_votes->execute(array('acteur_id'=>$_GET['acteur']));
+                        $vote_count = $nb_votes->fetch();
+
+                        $satisfaction = $bdd->prepare('SELECT AVG (vote) as vote_moyen FROM vote WHERE id_acteur = :acteur_id');
+                        $satisfaction->execute(array('acteur_id'=>$_GET['acteur']));
+                        $note = $satisfaction->fetch();
+                        echo '<p>'.round($note['vote_moyen']*100).'% d\'utilisateurs satisfaits ('.$vote_count['nb_votes'].' votes exprimés )</p>';
+               
+                    ?>
+                </div>
+<!-- Module like/dislike -->
+                <div class="vote" >
+                    <button type="button" class="like">
+                        <i class="fas fa-thumbs-up"></i>
+                    </button>
+                    <button type="button" class="dislike">
+                        <i class="fas fa-thumbs-down"></i>
+                    </button>
+                </div>
+
+            </div>
+
+<!-- Module affiche commentaires -->  
+            <div class="commentaires" >
+                <div><p> les 10 derniers commentaires : </p></div>
+                <?php
                         if ($comment_count['nb_commentaires'] == 0 ) {
-                            echo '<div class="col-12"><p class="text-center">Pas encore de commentaires pour cet acteur, mais n\'hésitez pas à en laisser un!</p></div>';
+                            echo '<div class="commentaire-none"><p>Pas encore de commentaires pour cet acteur, mais n\'hésitez pas à en laisser un!</p></div>';
                         }
                         else {
                             $requete = $bdd->prepare('SELECT id_post, id_user, id_acteur, DATE_FORMAT(date_add, \'%d/%m/%Y\') AS date, post FROM post WHERE id_acteur = :acteur_id ORDER BY id_post DESC LIMIT 0, 10');
                             $requete->execute(array('acteur_id'=>$_GET['acteur']));
                             while ($commentaires = $requete->fetch()) {
                                 $postAuteur=$commentaires['id_user'];
-                                ?>
-                                <div class="col-4 ">
-                                
-                                    <?php
-                                    $auteur = $bdd->prepare('SELECT username FROM account WHERE id_user = :user_id');
-                                    $auteur->execute(array('user_id'=>$postAuteur));
-                                    $pseudo = $auteur->fetch();
-                                    ?>
-                                    <p>Le <?php echo $commentaires['date'].' ';
-                                    echo '<strong>'.$pseudo['username'].'</strong> a écrit:</p>'; ?>
-                                </div>
-                                <div class="col-8 text-justify">
-                                    <p>
-                                        <?php echo $commentaires['post']; ?>
-                                    </p>
-                                </div>
-                            <?php
-                        }
-                    }
-                   ?> 
-                </div>
+                                $auteur = $bdd->prepare('SELECT username FROM account WHERE id_user = :user_id');
+                                $auteur->execute(array('user_id'=>$postAuteur));
+                                $pseudo = $auteur->fetch(); ?>
+                <div class="commentaire">                 
+                    <div class="pseudo" >
+                        <?php 
+                            echo '<div>'.ucfirst($pseudo['username']).'</div>'; 
+                        ?>
+                    </div>
+                    <div class="time" >
+                        <?php
+                            echo '<div>'.$commentaires['date'].'</div>';
+                        ?>
+                    </div>
+                    <div class="comment" >
+                        <?php echo '<div>'.$commentaires['post'].'</div>'; ?>
+                    </div>
+                </div><?php }} ?>
             </div>
-                   
-                   
-        </div>
-    </div>
-                    
-
-
-              
         
-   <?php include('footer.php'); ?>
+                            </div>
+
+
+<?php include('footer.php'); ?>
