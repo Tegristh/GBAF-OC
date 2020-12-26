a5cbab54d6cef6f2ae888c39b325ed8503520348
Baptiste Giannesini <b.giannesini@gmail.com>
2020-12-26 16:04:42 +0100

    W3C check

--- pages/footer.php
+++ pages/footer.php
@@ -10,14 +10,7 @@
                 |
             </p>
         </div>
-     <div>
-            <a href="http://jigsaw.w3.org/css-validator/check/referer">
-                <img style="border:0;width:88px;height:31px"
-                src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-                alt="Valid CSS!" />
-            </a>
-            <!-- certification html à passer -->
-    </div>
+    
         </footer>
         </div>
     <!-- fin footer -->
